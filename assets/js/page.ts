0187d64e4b9d1d3c442f9600282ba9500cd05d3d
Michael Reyna <MReyna12@live.com>
2023-10-05 16:57:56 -0500

    Adjust white space

--- assets/js/page.ts
+++ assets/js/page.ts
@@ -1,17 +1,17 @@
 export const resizePageHeight = (): void => {
-    const footer = document.querySelector<HTMLElement>('footer')
-    const pageClassElement = document.querySelector<HTMLElement>('.page')
-    let previousFooterHeight = footer.clientHeight
-    pageClassElement.style.height = `calc(100vh - var(--header-height) - ${previousFooterHeight}px)`
+  const footer = document.querySelector<HTMLElement>('footer')
+  const pageClassElement = document.querySelector<HTMLElement>('.page')
+  let previousFooterHeight = footer.clientHeight
+  pageClassElement.style.height = `calc(100vh - var(--header-height) - ${previousFooterHeight}px)`
 
-    const resizeObserver = new ResizeObserver((entries) => {
-        for (const entry of entries) {
-            if (previousFooterHeight !== entry.borderBoxSize[0].blockSize) {
-                pageClassElement.style.height = `calc(100vh - var(--header-height) - ${entry.borderBoxSize[0].blockSize}px)`
-                previousFooterHeight = entry.borderBoxSize[0].blockSize
-            }
-        }
-    })
+  const resizeObserver = new ResizeObserver((entries) => {
+    for (const entry of entries) {
+      if (previousFooterHeight !== entry.borderBoxSize[0].blockSize) {
+        pageClassElement.style.height = `calc(100vh - var(--header-height) - ${entry.borderBoxSize[0].blockSize}px)`
+        previousFooterHeight = entry.borderBoxSize[0].blockSize
+      }
+    }
+  })
     
-    resizeObserver.observe(footer)
+  resizeObserver.observe(footer)
 }
